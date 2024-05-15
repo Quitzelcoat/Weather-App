1013a02c5e2198383ce42a457e003ffffd943d45
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com.>
2024-05-15 16:46:02 +0500

    dom file setup

--- main.js
+++ main.js
@@ -2,6 +2,39 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/dom.js":
+/*!********************!*\
+  !*** ./src/dom.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   weatherDom: () => (/* binding */ weatherDom)
+/* harmony export */ });
+const weatherDom = () => {
+  function updateCurrentWeatherDom(currentWeather) {
+    const currentCelcius = document.querySelector(".currentCelcius");
+    currentCelcius.textContent = `${currentWeather.temperature} °C`;
+  }
+
+  function updateLocationWeatherDom(currentLocation) {}
+
+  function updateHourlyWeatherDom(hourlyForecast) {}
+
+  function updateDailyWeatherDom(dailyForecast) {}
+
+  return {
+    updateCurrentWeatherDom,
+    updateLocationWeatherDom,
+    updateHourlyWeatherDom,
+    updateDailyWeatherDom,
+  };
+};
+
+
+/***/ }),
+
 /***/ "./src/weather-api.js":
 /*!****************************!*\
   !*** ./src/weather-api.js ***!
@@ -13,7 +46,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   getWeatherData: () => (/* binding */ getWeatherData)
 /* harmony export */ });
 async function getWeatherData(location) {
-  let apiKey = "0d353533d1cd4029975135630240705";
+  let apiKey = "";
   try {
     const response = await fetch(
       `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=4&aqi=yes&alerts=yes`,
@@ -35,6 +68,7 @@ async function processWeatherData(data) {
   }
 
   const currentWeather = processCurrentWeather(data.current);
+  const weatherLocation = currentLocation(data.location);
   const hourlyForecast = processHourlyForecast(
     data.forecast.forecastday[0].hour
   );
@@ -44,6 +78,7 @@ async function processWeatherData(data) {
 
   return {
     current: currentWeather,
+    location: weatherLocation,
     hourly: hourlyForecast,
     daily: dailyForecast,
   };
@@ -78,6 +113,19 @@ async function processCurrentWeather(currentWeather) {
   return weatherInfo;
 }
 
+async function currentLocation(currentLocationData) {
+  const { country, localtime, name } = currentLocationData;
+
+  const LocationData = {
+    country: country,
+    time: localtime,
+    city: name,
+  };
+
+  console.log(LocationData);
+  return LocationData;
+}
+
 async function processHourlyForecast(hourlyData) {
   const next12Hours = hourlyData.slice(0, 13).map((hour) => ({
     time: hour.time,
@@ -88,6 +136,7 @@ async function processHourlyForecast(hourlyData) {
   console.log(next12Hours);
   return next12Hours;
 }
+
 async function processDailyForecast(dailyData) {
   const next3Days = dailyData.map((day) => ({
     date: day.date,
@@ -96,6 +145,7 @@ async function processDailyForecast(dailyData) {
     maxTemperature: day.day.maxtemp_c,
     minTemperature: day.day.mintemp_c,
   }));
+
   console.log(next3Days);
   return next3Days;
 }
@@ -167,13 +217,26 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _weather_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./weather-api.js */ "./src/weather-api.js");
+/* harmony import */ var _dom_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./dom.js */ "./src/dom.js");
 
+
+
+async function updateWeatherDom(location) {
+  const weatherData = await (0,_weather_api_js__WEBPACK_IMPORTED_MODULE_0__.getWeatherData)(location);
+  if (weatherData) {
+    (0,_dom_js__WEBPACK_IMPORTED_MODULE_1__.weatherDom)().updateCurrentWeatherDom(weatherData.current);
+    (0,_dom_js__WEBPACK_IMPORTED_MODULE_1__.weatherDom)().updateLocationWeatherDom(weatherData.location);
+    (0,_dom_js__WEBPACK_IMPORTED_MODULE_1__.weatherDom)().updateHourlyWeatherDom(weatherData.hourly);
+    (0,_dom_js__WEBPACK_IMPORTED_MODULE_1__.weatherDom)().updateDailyWeatherDom(weatherData.daily);
+  }
+}
 
 const locationBtn = document.getElementById("locationBtn");
 const weatherLoc = document.getElementById("weatherLoc");
 
-locationBtn.addEventListener("click", () => {
-  (0,_weather_api_js__WEBPACK_IMPORTED_MODULE_0__.getWeatherData)(weatherLoc.value);
+locationBtn.addEventListener("click", async () => {
+  const weatherData = await (0,_weather_api_js__WEBPACK_IMPORTED_MODULE_0__.getWeatherData)(weatherLoc.value);
+  updateWeatherDom(weatherData.current);
   weatherLoc.value = "";
 });
 
