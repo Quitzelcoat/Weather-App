f21702031be32fad26366bdea542043fc365c112
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com.>
2024-05-21 22:29:49 +0500

    dom file setup

--- src/dom.js
+++ src/dom.js
@@ -8,18 +8,16 @@ export function displayWeatherData(data) {
   // FRONT PAGE!
 
   // Display location info
-  document.querySelector(
-    ".todayTime"
-  ).textContent = `Local Time: ${data.location.time}`;
+  document.querySelector(".todayTime").textContent = `${data.location.time}`;
 
   document.querySelector(
     ".locationToday"
-  ).textContent = `Location: ${data.location.city}, ${data.location.country}`;
+  ).textContent = `${data.location.city}`;
 
   // Display current weather
   document.getElementById(
     "currentCelcius"
-  ).textContent = `Temperature: ${data.current.temperature}°C`;
+  ).textContent = `${data.current.temperature}°C`;
 
   if (data.daily.length > 0) {
     const today = data.daily[0];
@@ -32,17 +30,22 @@ export function displayWeatherData(data) {
   const todayHours = document.getElementById("todayHours");
   todayHours.innerHTML = "";
   data.hourly.forEach((hour) => {
+    const eachHourDiv = document.createElement("div");
     const todayHourDiv = document.createElement("div");
     const todayConditionDiv = document.createElement("div");
     const todayTimeDiv = document.createElement("div");
+
+    eachHourDiv.className = "eachHourDiv";
+    eachHourDiv.classList.add("insideHour");
 
     todayHourDiv.textContent = `${hour.temperature}°C`;
     todayConditionDiv.textContent = `${hour.condition}`;
     todayTimeDiv.textContent = `${hour.time}`;
 
-    todayHours.appendChild(todayHourDiv);
-    todayHours.appendChild(todayConditionDiv);
-    todayHours.appendChild(todayTimeDiv);
+    eachHourDiv.appendChild(todayHourDiv);
+    eachHourDiv.appendChild(todayConditionDiv);
+    eachHourDiv.appendChild(todayTimeDiv);
+    todayHours.appendChild(eachHourDiv);
   });
 
   // BACK PAGE!
